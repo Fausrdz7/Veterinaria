c8107c3193ff9de214a38280b0f80fa569f6a0cb
Fausto Rodriguez <fasrodriguez9@gmail.com>
2024-05-21 16:40:06 -0300

    complete las clases, con sus atributos, cree enum y algunos constructores;

--- Model/src/Diagnostico.java
+++ Model/src/Diagnostico.java
@@ -1,2 +1,9 @@
 public class Diagnostico {
+private Mascota mascota;
+private Turno turno;
+private  String detalles;
+private Insumo insumo;
+private String receta;
+
+
 }
--- Model/src/Veterinario.java
+++ Model/src/Veterinario.java
@@ -1,2 +1,8 @@
-public class Veterinario {
+public class Veterinario extends Usuario{
+    private String matricula;
+
+    public Veterinario(String nombreUsuario, String contraseña, String dni, Role role, String nombre, String apellido, String matricula) {
+        super(nombreUsuario, contraseña, dni, role, nombre, apellido);
+        this.matricula = matricula;
+    }
 }
