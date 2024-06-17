1abded4d07ecae4ad86daf97a26c5fd78c69a04b
Luciano Delmonte <lucianodelmonte00@gmail.com>
2024-06-17 17:30:52 -0300

    coleccion mascota y coleccion administrador

---
+++ Model/src/Colleciones/ColeccionMascota.java
@@ -0,0 +1,28 @@
+package Colleciones;
+
+import Entidades.Administrador;
+import Entidades.Mascota;
+import Entidades.Veterinario;
+
+import java.util.List;
+
+public class ColeccionMascota {
+    private final Coleccion<Mascota,String,String>coleccionMascota = new Coleccion<>();
+
+    public boolean agregarMascota(Mascota mascota) {
+        return coleccionMascota.agregar(mascota);
+    }
+
+    public Mascota buscarMascota(String dni) throws Exception {
+        return coleccionMascota.buscar(dni);
+    }
+
+    public List<Mascota> filtrarAdministrador(String dni) {
+        return coleccionMascota.filtrar(dni);
+    }
+
+    public boolean eliminarAdministrador(String dni) throws Exception {
+        Mascota mascota = buscarMascota(dni);
+        return coleccionMascota.eliminar(mascota);
+    }
+}
--- Model/src/Colleciones/ColeccionVeterinarios.java
+++ Model/src/Colleciones/ColeccionVeterinarios.java
@@ -4,18 +4,17 @@ import Entidades.Veterinario;
 import Exepciones.ElementoNoEncontradoException;
 
 public class ColeccionVeterinarios {
-    private final Coleccion<Veterinario,String,String>coleccionVeterinario=new Coleccion<>();
+    private final Coleccion<Veterinario,String,String>coleccionVeterinario = new Coleccion<>();
 
     public Veterinario buscarVeterinario(String matricula)  {
         try {
             return coleccionVeterinario.buscar(matricula);
-        } catch (ElementoNoEncontradoException e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;        }
     }
     public boolean agregar (Veterinario veterinario){
        return coleccionVeterinario.agregar(veterinario);
     }
-    public
 
 }
